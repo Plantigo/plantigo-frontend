cac5ee7b93a58992a4a1388ef07d5a275ed0fa35
jaqb8 <jakubaniszewski@pm.me>
2024-12-21 22:11:58 +0100

    feat: add error handling for authentication functions and improve docker-compose formatting

--- app/routes/_auth/actions.ts
+++ app/routes/_auth/actions.ts
@@ -1,44 +1,65 @@
 import { API_BASE_URL } from "@/lib/env.server";
 
 export async function login(data: Record<string, string>) {
-  const response = await fetch(`${API_BASE_URL}/api/v1/auth/token/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: data.email,
-      password: data.password,
-    }),
-  });
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/v1/auth/token/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+      }),
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    console.error("Login error:", error);
+    return new Response(JSON.stringify({ error: "Server error" }), {
+      status: 500,
+    });
+  }
 }
 
 export async function refreshAccessToken(data: Record<string, string>) {
-  return await fetch(`${API_BASE_URL}/api/v1/auth/token/refresh/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      refresh: data.refresh,
-    }),
-  });
+  try {
+    return await fetch(`${API_BASE_URL}/api/v1/auth/token/refresh/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: data.refresh,
+      }),
+    });
+  } catch (error) {
+    console.error("Refresh token error:", error);
+    return new Response(JSON.stringify({ error: "Server error" }), {
+      status: 500,
+    });
+  }
 }
 
 export async function registerUser(data: Record<string, string>) {
-  const response = await fetch(`${API_BASE_URL}/api/v1/auth/register/`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: data.email,
-      password: data.password,
-      confirm_password: data.confirmPassword,
-    }),
-  });
+  try {
+    const response = await fetch(`${API_BASE_URL}/api/v1/auth/register/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: data.email,
+        password: data.password,
+        confirm_password: data.confirmPassword,
+      }),
+    });
 
-  return response;
+    return response;
+  } catch (error) {
+    console.error("Register error:", error);
+    return new Response(JSON.stringify({ error: "Server error" }), {
+      status: 500,
+    });
+  }
 }
