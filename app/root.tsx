20ae63e3001ae2edeb693519be4cd22c847536bd
jaqb8 <jakubaniszewski@pm.me>
2024-12-21 22:34:15 +0100

    feat: add custom error boundary component with user-friendly message and navigation

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  Link,
   Links,
   Meta,
   Outlet,
@@ -9,6 +10,9 @@ import type { LinksFunction } from "@remix-run/node";
 
 import styles from "./tailwind.css?url";
 import { Toaster } from "@/components/ui/toaster";
+import { Card } from "./components/ui/card";
+import { AlertCircle } from "lucide-react";
+import { Button } from "./components/ui/button";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -47,4 +51,40 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function App() {
   return <Outlet />;
+}
+
+export function ErrorBoundary() {
+  return (
+    <div className="px-6 absolute top-0 left-0 right-0 bottom-0 flex items-center justify-center">
+      <Card className="p-6">
+        <div className="flex flex-col items-center gap-4">
+          <div className="flex justify-center">
+            <div className="bg-red-100 p-3 rounded-full">
+              <AlertCircle size="48" className="text-red-600" />
+            </div>
+          </div>
+
+          <h1 className="text-3xl font-bold text-center text-gray-900">
+            Something went wrong!
+          </h1>
+
+          <p className="text-gray-600 text-center  leading-relaxed">
+            An error occurred while trying to render this page. Please try again
+            later.
+          </p>
+
+          <div className="pt-4">
+            <Button
+              variant="default"
+              size="lg"
+              className="rounded-full"
+              asChild
+            >
+              <Link to="/">Go to Home</Link>
+            </Button>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
 }
